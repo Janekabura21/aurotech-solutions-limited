3140bf336543c701ccf959ea893d1af8944369ec
Janekabura21 <janenjoki2105@gmail.com>
2026-03-10 08:45:55 +0300

    hello

--- machineshop/settings.py
+++ machineshop/settings.py
@@ -32,6 +32,8 @@ CLOUDINARY_STORAGE = {
     "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
 }
 
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
 STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
